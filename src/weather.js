0294d5a137885268c5978e3ebcf3a1aaaed01182
imahnama <muannama25@gmail.com>
2020-08-20 12:53:22 +0300

    craete file weather

---
+++ src/weather.js
@@ -0,0 +1,19 @@
+// import mainPage from './dom'
+//
+// const getWeather =  async() => {
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=b4798b30d3b96afc9bb209e0110b6378')
+//     .then(response => response.json())
+//     .then(data => {
+//       const nameValue = data['name']
+//       const tempValue = data['main']['temp']
+//       const descValue = data['weather'][0]['description']
+//
+//       cityName.innerHTML = nameValue
+//       cityDesc.innerHTML = descValue
+//       cityTemp.innerHTML = tempValue
+//     })
+//
+//   }
+//
+//
+// export default getWeather;
